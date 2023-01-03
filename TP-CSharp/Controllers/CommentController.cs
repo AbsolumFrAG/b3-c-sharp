9acff90e271c459f3907fd908f249946161d0eb6
Lou Tigroudja <lou.tigroudja07@gmail.com>
2023-01-03 14:14:14 +0100

    Ajout d'une propriété Message dans l'ErrorViewModel ainsi qu'un Controller pour la gestion d'erreur 404

---
+++ TP-CSharp/Controllers/CommentController.cs
@@ -0,0 +1,42 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TP_CSharp.Models;
+
+namespace TP_CSharp.Controllers
+{
+    public class CommentController : Controller
+    {
+        private readonly MainContext _context;
+
+        public CommentController(MainContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public PartialViewResult Create()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Comment comment)
+        {
+            if (!ModelState.IsValid) return View(comment);
+            var userId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier) ?? string.Empty);
+
+            var user = _context.Users.SingleOrDefault(u => u.Id == userId);
+
+            comment.UserId = user?.Id;
+
+            comment.CreatedAt = DateTime.UtcNow;
+            _context.Add(comment);
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Detail", "Blog", new { id = comment.BlogId });
+        }
+    }
+}
---
+++ TP-CSharp/Controllers/ErrorController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TP_CSharp.Models;
+
+namespace TP_CSharp.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public IActionResult NotFound()
+        {
+            var model = new ErrorViewModel
+            {
+                RequestId = HttpContext.TraceIdentifier,
+                Message = "Désolé, la page demandée n'est pas disponible.",
+            };
+            return View(model);
+        }
+    }
+}
