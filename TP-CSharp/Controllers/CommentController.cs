c2557f8e0297d8734f22c21e45f662ede69729bb
Lou Tigroudja <lou.tigroudja07@gmail.com>
2023-01-03 17:01:04 +0100

    Controller gérant les commentaires

--- TP-CSharp/Controllers/CommentController.cs
+++ TP-CSharp/Controllers/CommentController.cs
@@ -1,6 +1,7 @@
-﻿using System.Security.Claims;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 using TP_CSharp.Models;
 
 namespace TP_CSharp.Controllers
@@ -32,9 +33,75 @@ namespace TP_CSharp.Controllers
 
             comment.UserId = user?.Id;
 
-            comment.CreatedAt = DateTime.UtcNow;
+            comment.CreatedAt = DateTime.Now;
             _context.Add(comment);
 
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Detail", "Blog", new { id = comment.BlogId });
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var comment = await _context.Comments.SingleOrDefaultAsync(m => m.CommentId == id);
+
+            var userId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier) ?? string.Empty);
+
+            if (comment != null && !comment.UserId.Equals(userId))
+                if (!User.IsInRole("admin"))
+                    return RedirectToAction("AccessDenied", "Home");
+
+            var model = new UpdateBlogViewModel
+            {
+                Content = comment?.Content
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> Edit(int id, UpdateCommentViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var comment = await _context.Comments.SingleOrDefaultAsync(m => m.CommentId == id);
+
+            var userId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier) ?? string.Empty);
+
+            if (comment != null && !comment.UserId.Equals(userId))
+            {
+                if (!User.IsInRole("admin"))
+                    return RedirectToAction("AccessDenied", "Home");
+            }
+
+            comment.Content = model.Content;
+            await _context.SaveChangesAsync();
+
+            return User.IsInRole("admin") ? RedirectToAction("Comments", "Admin") : RedirectToAction("Detail", "Blog", new { id = comment.BlogId });
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return RedirectToAction("NotFound", "Error");
+
+            var comment = await _context.Comments
+                .SingleOrDefaultAsync(m => m.CommentId == id);
+
+            if (comment == null)
+                return RedirectToAction("NotFound", "Error");
+
+            var userId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier) ?? string.Empty);
+
+            if (!comment.UserId.Equals(userId))
+                if (!User.IsInRole("admin"))
+                    return RedirectToAction("AccessDenied", "Home");
+
+            _context.Comments.Remove(comment);
             await _context.SaveChangesAsync();
             return RedirectToAction("Detail", "Blog", new { id = comment.BlogId });
         }
